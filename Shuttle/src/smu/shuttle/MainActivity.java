5b9fb54236da22ed81d19fc7e50bf8b187364bcb
Hong <Hong@DESKTOP-9UMQ5B9>
2016-11-17 21:48:24 +0900

    변수 선언

--- Shuttle/src/smu/shuttle/MainActivity.java
+++ Shuttle/src/smu/shuttle/MainActivity.java
@@ -4,9 +4,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+	TextView tv;
+	Button btn;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
