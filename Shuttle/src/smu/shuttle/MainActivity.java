f3085bcce2d93c0b337bb5f22d97a236e41b502e
Hong <Hong@192.168.1.70>
2016-11-24 23:52:28 +0900

    액티비티 전환

--- Shuttle/src/smu/shuttle/MainActivity.java
+++ Shuttle/src/smu/shuttle/MainActivity.java
@@ -1,21 +1,36 @@
 package smu.shuttle;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 	TextView tv;
-	Button btn;
+	Button totalTimeBtn, nearTimeBtn;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		totalTimeBtn = (Button) findViewById(R.id.totalTimeBtn);
+		nearTimeBtn = (Button) findViewById(R.id.nearTimeBtn);
+		
+		totalTimeBtn.setOnClickListener(this);
+		nearTimeBtn.setOnClickListener(this);
 	}
-
+	
+	public void onClick(View v){
+		Toast.makeText(this, "토탈 타임클릭", Toast.LENGTH_SHORT).show();
+		Intent i = new Intent(this, TotalTimeActivity.class);
+		startActivity(i);
+	}
 	
 }
