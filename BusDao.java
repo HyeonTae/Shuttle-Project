1ee28ea974777ffb842837bd722144ddc6e5fb87
HyeonTae-PC\dchs4 <dchs504@gmail.com>
2016-11-29 17:06:24 +0900

    버스시간표 id값으로 조회 (수정조회하기위함)

--- BusDao.java
+++ BusDao.java
@@ -82,6 +82,30 @@ public class BusDao {
 		}
 		return null;
 	}
+	
+	// 버스시간표 조회 (id)
+	public Bus getSearchBusForId(String id) {
+		con = DBUtil.getConnection();
+		String sql = "select * from bus where id=?";
+		pst = null;
+		rs = null;
+		try {
+			pst = con.prepareStatement(sql);
+			pst.setString(1, id);
+			rs = pst.executeQuery();
+			rs.next();
+			
+			return new Bus(rs.getString(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getInt(5));
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			DBUtil.close(con);
+			DBUtil.close(pst);
+			DBUtil.close(rs);
+		}
+		return null;
+	}
 
 	// 버스시간표 조회 (학교->목적지)
 	public ArrayList<Bus> getSearchBusToDest(Bus b) {
