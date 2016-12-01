e193521a85f9d3d7e9ff7081505f861383d55f1b
Hong <Hong@DESKTOP-9UMQ5B9>
2016-12-01 23:52:29 +0900

    탭바 구현(gps 적용x)

--- Shuttle/src/smu/shuttle/MainActivity.java
+++ Shuttle/src/smu/shuttle/MainActivity.java
@@ -1,92 +1,72 @@
 package smu.shuttle;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.ActionBar.TabListener;
 import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.text.format.DateFormat;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import smu.custom.CustomAdapter;
 
 public class MainActivity extends Activity {
-	final LocationManager lm= (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-	TextView tv,tv5;
-	Button totalTimeBtn, nearTimeBtn;
-	//현재시간을 msec으로 구한다
-	long now = System.currentTimeMillis();
-	Date date = new Date(now);
-	SimpleDateFormat dateFomat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-	String formatDate = dateFomat.format(date);
+	ViewPager pager; 
+	ActionBar actionBar; 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		actionBar = getActionBar();
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 		
-		totalTimeBtn = (Button) findViewById(R.id.totalTimeBtn);
-		nearTimeBtn = (Button) findViewById(R.id.nearTimeBtn);
-		tv = (TextView) findViewById(R.id.textView1);
-		tv = (TextView) findViewById(R.id.textView5);
-		
-		tv.setText(formatDate);
-		tv5.setText("위치정보 수신중");
+		//페이저 (밀어서 화면 전환)
+		pager = (ViewPager) findViewById(R.id.pager);
+		CustomAdapter adapter = new CustomAdapter(getLayoutInflater());
+		pager.setAdapter(adapter);
+		pager.setOnPageChangeListener(new OnPageChangeListener() {
+			@Override
+			public void onPageSelected(int position) {
+				// TODO Auto-generated method stub
+				actionBar.setSelectedNavigationItem(position);
+			}
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+				// TODO Auto-generated method stub
+			}
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+				// TODO Auto-generated method stub
+			}
+		});
+		Tab tab = null;
+		tab = actionBar.newTab();
+		tab.setText("가장 가까운 시간표"); 
+		tab.setTabListener(listener);
+		actionBar.addTab(tab);
+		tab = actionBar.newTab(); 
+		tab.setText("천안/아산역"); 
+		tab.setTabListener(listener);
+		actionBar.addTab(tab);
+		tab = actionBar.newTab(); 
+		tab.setText("터미널"); 
+		tab.setTabListener(listener);
+		actionBar.addTab(tab);
 	}
-	
-	public void totalTimeBtn(View v){
-		Toast.makeText(this, "토탈 타임클릭", Toast.LENGTH_SHORT).show();
-		Intent i = new Intent(this, TotalTimeActivity.class);
-		startActivity(i);
-	}
-	public void nearTimeBtn(View v){
-		tv5.setText("수신중..");
-		
-		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, 1, mLocationListenr);
-		lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 100, 1, mLocationListenr);
-		
-	}
-	private final LocationListener mLocationListenr = new LocationListener() {
-		
+	TabListener listener = new TabListener() {
 		@Override
-		public void onStatusChanged(String provider, int status, Bundle extras) {
+		public void onTabUnselected(Tab tab, FragmentTransaction ft) {
 			// TODO Auto-generated method stub
-			Log.d("locationtest", provider+status+extras);
 		}
-		
 		@Override
-		public void onProviderEnabled(String provider) {
+		public void onTabSelected(Tab tab, FragmentTransaction ft) {
 			// TODO Auto-generated method stub
-			Log.d("locationtest", "인에이블"+provider);
+			int position = tab.getPosition();
+			pager.setCurrentItem(position, true);
 		}
-		
 		@Override
-		public void onProviderDisabled(String provider) {
+		public void onTabReselected(Tab tab, FragmentTransaction ft) {
 			// TODO Auto-generated method stub
-			Log.d("locationtest", "디스어블"+provider);
-		}
-		
-		@Override
-		public void onLocationChanged(Location location) {
-			// TODO Auto-generated method stub
-			double longitude = location.getLongitude();//경도
-			double latitude = location.getLatitude();//위도
-			double altitude = location.getAltitude();//고도
-			float accuracy = location.getAccuracy();//정확도
-			String provider = location.getProvider();//위치제공자
-			
-			tv5.setText("현재 위치 정보:"+provider+"\n위도:"+latitude+"\n경도"+longitude+"\n고도:"+altitude+"\n정확도:"+accuracy+"\n위치제공자:"+provider);
-			
-			
 		}
 	};
 }
