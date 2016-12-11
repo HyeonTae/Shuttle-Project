ad3b41af543f1d381099ab9728bdbceea11415f2
HyeonTae <dchs401@naver.com>
2016-12-11 23:43:31 +0900

    Create Class.java

---
+++ Shuttle/src/smu/model/Class.java
@@ -0,0 +1,65 @@
+package smu.shuttle.model;
+
+public class Class {
+	private String id;
+	private String pass;
+	private String name;
+	private String dept;
+	private String area;
+	
+	public Class(String id, String pass, String name, String dept, String area) {
+		super();
+		this.id = id;
+		this.pass = pass;
+		this.name = name;
+		this.dept = dept;
+		this.area = area;
+	}
+	
+
+	@Override
+	public String toString() {
+		return "Class [id=" + id + ", pass=" + pass + ", name=" + name + ", dept=" + dept + ", area=" + area + "]";
+	}
+
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public String getArea() {
+		return area;
+	}
+
+	public void setArea(String area) {
+		this.area = area;
+	}
+}
